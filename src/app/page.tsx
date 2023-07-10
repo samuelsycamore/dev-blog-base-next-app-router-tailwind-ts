7097336b022f9e74354a66764ebb972beffce4bc
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-07-10 14:16:59 -0500

    initializing

---
+++ src/app/page.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div className="h-full bg-black bg-gradient-to-b from-black to-blue-900/30">
+      <div className="mx-auto max-w-xl py-16">
+        <div className="flex items-center gap-4">
+          <img src="https://ca.slack-edge.com/TS83SPSTV-U031Q4073B3-dc9dc81764a8-512" className="h-12 w-12 rounded-full grayscale" />
+          <span>
+            <h1 className="mb-1 text-2xl font-semibold text-white">Sam's blog</h1>
+            <p className="text-gray-500">Built using Base UI, Tailwind CSS, and Next.js.</p>
+          </span>
+        </div>
+        <hr className="mt-8 border-gray-800/70" />
+        <ul className="flex flex-col gap-10 py-8">
+          <li className="-m-5 cursor-pointer rounded-lg border border-gray-800/0 p-5 transition hover:border-gray-800/60 hover:bg-gray-900/60">
+            <div className="flex items-baseline justify-between">
+              <span className="flex items-center gap-1.5">
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-gray-400/70">Opinion</p>
+                <span className="text-gray-700">&bull;</span>
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-yellow-400/50">Article</p>
+              </span>
+              <p className="mb-1 text-xs text-gray-500">July 7, 2023</p>
+            </div>
+            <h2 className="mb-1 text-white">Building hype on Twitter through fake apps</h2>
+            <p className="mb-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a className="text-sm font-semibold text-blue-400">Read more &rarr;</a>
+          </li>
+          <li className="-m-5 cursor-pointer rounded-lg border border-gray-800/0 p-5 transition hover:border-gray-800/60 hover:bg-gray-900/60">
+            <div className="flex items-baseline justify-between">
+              <span className="flex items-center gap-1.5">
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-gray-400/70">Opinion</p>
+                <span className="text-gray-700">&bull;</span>
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-yellow-400/50">Article</p>
+              </span>
+              <p className="mb-1 text-xs text-gray-500">July 7, 2023</p>
+            </div>
+            <h2 className="mb-1 text-white">Building hype on Twitter through fake apps</h2>
+            <p className="mb-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a className="text-sm font-semibold text-blue-400">Read more &rarr;</a>
+          </li>
+          <li className="-m-5 cursor-pointer rounded-lg border border-gray-800/0 p-5 transition hover:border-gray-800/60 hover:bg-gray-900/60">
+            <div className="flex items-baseline justify-between">
+              <span className="flex items-center gap-1.5">
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-gray-400/70">Opinion</p>
+                <span className="text-gray-700">&bull;</span>
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-yellow-400/50">Article</p>
+              </span>
+              <p className="mb-1 text-xs text-gray-500">July 7, 2023</p>
+            </div>
+            <h2 className="mb-1 text-white">Building hype on Twitter through fake apps</h2>
+            <p className="mb-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a className="text-sm font-semibold text-blue-400">Read more &rarr;</a>
+          </li>
+          <li className="-m-5 cursor-pointer rounded-lg border border-gray-800/0 p-5 transition hover:border-gray-800/60 hover:bg-gray-900/60">
+            <div className="flex items-baseline justify-between">
+              <span className="flex items-center gap-1.5">
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-gray-400/70">Opinion</p>
+                <span className="text-gray-700">&bull;</span>
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-yellow-400/50">Article</p>
+              </span>
+              <p className="mb-1 text-xs text-gray-500">July 7, 2023</p>
+            </div>
+            <h2 className="mb-1 text-white">Building hype on Twitter through fake apps</h2>
+            <p className="mb-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a className="text-sm font-semibold text-blue-400">Read more &rarr;</a>
+          </li>
+          <li className="-m-5 cursor-pointer rounded-lg border border-gray-800/0 p-5 transition hover:border-gray-800/60 hover:bg-gray-900/60">
+            <div className="flex items-baseline justify-between">
+              <span className="flex items-center gap-1.5">
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-gray-400/70">Opinion</p>
+                <span className="text-gray-700">&bull;</span>
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-yellow-400/50">Article</p>
+              </span>
+              <p className="mb-1 text-xs text-gray-500">July 7, 2023</p>
+            </div>
+            <h2 className="mb-1 text-white">Building hype on Twitter through fake apps</h2>
+            <p className="mb-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a className="text-sm font-semibold text-blue-400">Read more &rarr;</a>
+          </li>
+          <li className="-m-5 cursor-pointer rounded-lg border border-gray-800/0 p-5 transition hover:border-gray-800/60 hover:bg-gray-900/60">
+            <div className="flex items-baseline justify-between">
+              <span className="flex items-center gap-1.5">
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-gray-400/70">Opinion</p>
+                <span className="text-gray-700">&bull;</span>
+                <p className="text-[10px] font-semibold uppercase tracking-wider text-yellow-400/50">Article</p>
+              </span>
+              <p className="mb-1 text-xs text-gray-500">July 7, 2023</p>
+            </div>
+            <h2 className="mb-1 text-white">Building hype on Twitter through fake apps</h2>
+            <p className="mb-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a className="text-sm font-semibold text-blue-400">Read more &rarr;</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
